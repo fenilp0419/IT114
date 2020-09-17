a4f440a2d8b7cff1be144da5ba7349b3310849c3
Fenil Patel <fenilp0419@gmail.com>
2020-09-17 14:28:49 -0400

    HW JavaData

---
+++ Basics/JavaData/src/JavaData.java
@@ -0,0 +1,29 @@
+public class JavaData {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		
+		String name = "Fenil";
+
+		// 2. Output the name to the console
+		
+		System.out.println("My name is " + name);
+
+		// 3. Create a variable that holds an age (as a whole number)
+		
+		int x = 22;
+
+		// 4. Output the age to the console
+		
+		System.out.println("My age is " + x);
+
+		// 5. Create a variable that holds a decimal
+		
+		float y = 22.5f;
+
+		// 6. Output the variable
+				
+		System.out.println("Decimal is " + y);
+		
+	}
+}
